b2e7c96661affc3c09c40899c73281df5a3019d3
anuj-weebjs <weeb.js@yahoo.com>
2024-01-20 16:49:07 +0530

    Wrote The Basic Logic

---
+++ clearClutter.js
@@ -0,0 +1,48 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const config= require('./config.json');
+
+const folder = config.folderName;
+console.log(folder)
+const folderPath = path.join(__dirname, folder);
+try{
+var folderFiles = fs.readdirSync(folder);
+}catch(err){
+console.error(`There was an Error While Moving Files or There are no such files to move.`);
+process.exit();
+}
+let movedFiles = 0;
+let createdFolders = 0;
+
+for (const file of folderFiles) {
+    const ext = file.split('.')[file.split('.').length - 1];
+
+    let oldPath = path.join(folderPath, file);
+    let newPath = path.join(folderPath, ext, file);
+    let extensionPath = path.join(folderPath, ext);
+
+    if (!fs.existsSync(path.join(folderPath, ext))) {
+        try {
+            fs.mkdirSync(extensionPath);
+            console.log(`Created ${ext} Folder`);
+            createdFolders++;
+        } catch (error) {
+            console.log(`There wan error Creating ${ext} Folder/n ${error}`);
+        }
+    }
+    try {
+        fs.renameSync(oldPath, newPath);
+        console.log(`SuccessFully Moved ${file}`);
+        movedFiles++;
+    } catch (error) {
+        console.log(`There was an Error While Moving ${file} file`)
+    }
+}
+
+if (movedFiles == 0) {
+    console.error(`There was an Error While Moving Files or There are no such files to move.`)
+} else {
+    console.log(`Successfully Moved ${movedFiles} Files.`)
+}
+
+
